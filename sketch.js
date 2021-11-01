cf49583671e7150ee194ddb5d000079293dd1f81
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-11-01 17:25:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,7 @@
 var box;
 function setup()
 {
- createCanvas(400,400)
- box = createSprite(40,40,40,40);
- box.shapeColor = color(255);
- box.x=  ;
- box.y=150;
-
+ createCanvas(400,400);
 }
 
 function draw()
